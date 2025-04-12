ce4db6ec0ddeb3bb4cbe710fb6cefd3e1aa2d93a
annah <annalutonde@gmail.com>
2025-04-12 13:34:33 +0200

    gestion de la connexion

---
+++ Gestion_personne_groupe6/GestionDeLaConnexion/GestionDeLaConnexion/connexion.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeLaConnexion
+{
+    public class connexion
+    {
+
+        string _serveur = "serveur";
+        string _user = "user";
+        string _password = "password";
+        string _database = "database";
+
+
+        public string Serveur
+        {
+            get
+            {
+                return _serveur;
+            }
+
+            set
+            {
+                _serveur = value;
+            }
+        }
+        public string Database
+        {
+            get
+            {
+                return _database;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new InvalidOperationException("Please specify a valid Database !!!");
+                else
+                    _database = value;
+            }
+        }
+
+
+        public string User
+        {
+            get
+            {
+                return _user;
+            }
+
+            set
+            {
+                _user = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+
+            set
+            {
+                _password = value;
+            }
+        }
+
+
+    }
+}
---
+++ Gestion_personne_groupe6/GestionDeLaConnexion/ImplementerConnexion.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeLaConnexion
+{
+    public class ImplementerConnexion:IConnexion
+    {
+
+        public static SqlConnection con = new SqlConnection();
+        public static SqlCommand cmd = new SqlCommand();
+        private SqlDataAdapter sda = new SqlDataAdapter();
+        //private DataTable dt = new DataTable();
+        public string constr;
+        private static ImplementerConnexion instance;
+
+
+        public ImplementerConnexion()
+        {
+            //constr = connectivite();
+            //con = new SqlConnection(constr);
+            //cmd = new SqlCommand();
+           // cmd.Connection = con;
+        }
+        private string connectivite(connexion conex)
+        {
+            string nomserveur = conex.Serveur;
+            string nombd = conex.Database;
+            string nomuser = conex.User;
+            string mtp = conex.Password;
+            constr = string.Format("Data Source={0};Initial Catalog={1};User ID={2};PassWord={3};MultipleActiveResultSets=true", nomserveur, nombd, nomuser, mtp);
+            return constr;
+        }
+        public static ImplementerConnexion getinstance()
+        {
+            if (instance == null) instance = new ImplementerConnexion();
+            return instance;
+        }
+        public void connections(connexion conex)
+        {
+            constr = connectivite(conex);
+            con = new SqlConnection(constr);
+            if (con.State == ConnectionState.Closed)
+
+                con.Open();
+        }
+        public static bool connectioncreer()
+        {
+            if (con.State == ConnectionState.Closed)
+
+                con.Open();
+            return true;
+        }
+
+        public static bool fermerconnection()
+        {
+            if (con.State == ConnectionState.Open)
+
+                con.Close();
+            return true;
+        }
+    }
+}
