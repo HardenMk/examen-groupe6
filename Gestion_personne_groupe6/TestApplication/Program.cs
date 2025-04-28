726008490ed11d62dad46ffdff05369fcdcb9557
groupe12 <groupe12@gmail.com>
2025-04-28 12:15:57 +0200

    test composant application

---
+++ Gestion_personne_groupe6/TestApplication/Program.cs
@@ -0,0 +1,44 @@
+﻿using GestionDeLaConnexion;
+using System;
+using System.Data;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                connexion connection = new connexion();
+
+                connection.Serveur = @"INGENIEUR";
+                connection.Database = "gestion_personne";
+                connection.User = "sa";
+                connection.Password = "mk101214";
+                ImplementerConnexion.getinstance().connections(connection);
+
+                Console.WriteLine("Connection Successfuly !!!");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("Connection error, " + ex.Message);
+            }
+            //catch (System.Data.SqlClient ex)
+            //{
+            //    Console.WriteLine("Connection error, " + ex.Message);
+            //}
+            catch (Exception ex)
+            {
+                Console.WriteLine("Connection error, " + ex.Message);
+            }
+            finally
+            {
+                ImplementerConnexion.fermerconnection();
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
